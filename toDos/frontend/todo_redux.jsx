09b203eae618e0aeeae3354d36ae3bbdfdfc25cb
Eric Tran <eric.tran119@gmail.com>
2021-06-22 17:00:32 -0400

    finished part 1 :)

--- toDos/frontend/todo_redux.jsx
+++ toDos/frontend/todo_redux.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store'
 import {receiveTodos, receiveTodo, removeTodo} from './actions/todo_actions'
+import { receiveSteps, receiveStep, removeStep } from './actions/steps_actions'
 
 document.addEventListener('DOMContentLoaded', () => {
     window.store = configureStore();
     window.receiveTodos = receiveTodos;
     window.receiveTodo = receiveTodo;
     window.removeTodo = removeTodo;
+    window.receiveSteps = receiveSteps;
+    window.receiveStep = receiveStep;
+    window.removeStep = removeStep;
   })
+
+
